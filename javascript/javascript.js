2b108b93f98d6d13a2356b01c51330a9e544a194
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2021-12-05 17:45:53 -0600

    finished styling services section + added js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,3 +8,18 @@
       x.className = "navbar";
     }
   }
+
+  //About Page Random Images
+
+
+    var imageUrls = [
+        "images/about1.jpg"
+    ,   "images/about2.jpg"
+    ,   "images/about3.jpg"
+    ];
+
+    var rand_int = Math.floor(Math.random() * 3);
+
+    var image = document.getElementById("image-about")
+
+    image.src = imageUrls[rand_int];
